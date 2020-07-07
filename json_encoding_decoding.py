6710cb8a8d54eb23b0406b3af99c2cd98a22034a
ibbocus <ib_bocus@hotmail.com>
2020-07-07 10:56:57 +0100

    encoding/decoding

---
+++ json_encoding_decoding.py
@@ -0,0 +1,25 @@
+import json
+
+car_data = {"name": "tesla", "engine":"electric"} # creating a dictionary
+
+print(car_data) # printing the dictionary
+
+print(type(car_data)) # checking the type of the dictionary
+
+car_data_json_string = json.dumps(car_data)
+# json.dumps changes python dict to json str dumps = dump string
+
+print(type((car_data_json_string)))
+
+with open("new_json_file.json", "w") as jsonfile: # enter the name of the file, make it writable using "w"
+    json.dump(car_data, jsonfile)
+    # dump takes two arguments, the first is the dictionary the second is the file object to dump it to
+
+# encoding and creating and writing into json file
+
+with open("new_json_file.json") as jsonfile:
+    # reading from the file we just created
+    car = json.load(jsonfile)
+    print(type(car_data)) # returns a dictionary, as only dumps converts to string whereas dump puts a json object somewhere
+    print(car['name'])
+    print(car['engine']) #since this is in the with open block, you know that this is coming from the new
